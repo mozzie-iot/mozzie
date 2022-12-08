bb867023f27e6291472db098bfbffefe0a77fdf3
Tyler Smith <tilersmyth@gmail.com>
2022-12-08 19:07:40 +0000

    add resolver

--- packages/api/src/routes/modules/instance/instance.module.ts
+++ packages/api/src/routes/modules/instance/instance.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { NativeClientModule } from '@huebot-api/native-client/native-client.module';
 
+import { InstanceResolver } from './instance.resolver';
 import { InstanceService } from './instance.service';
 
 @Module({
   imports: [NativeClientModule],
-  providers: [InstanceService],
+  providers: [InstanceService, InstanceResolver],
 })
 export class InstanceModule {}
