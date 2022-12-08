29488af7bce85b33e40a69779c98a00a5c06de8d
Tyler Smith <tilersmyth@gmail.com>
2022-12-08 16:06:38 +0000

    Update instance setup
    Convert setup to transaction, save user with instance public key

--- packages/api/src/routes/modules/instance/instance.module.ts
+++ packages/api/src/routes/modules/instance/instance.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { NativeClientModule } from '@huebot-api/native-client/native-client.module';
-import { ConfigEntityModule } from '@huebot-hub-core/common';
 
 import { InstanceService } from './instance.service';
 
 @Module({
-  imports: [NativeClientModule, ConfigEntityModule],
+  imports: [NativeClientModule],
   providers: [InstanceService],
 })
 export class InstanceModule {}
--- packages/api/src/routes/modules/instance/instance.service.ts
+++ packages/api/src/routes/modules/instance/instance.service.ts
@@ -2,17 +2,18 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import jwt from 'jsonwebtoken';
 import { firstValueFrom } from 'rxjs';
+import { DataSource } from 'typeorm';
 
 import { NATIVE_CLIENT_PROVIDER } from '@huebot-api/native-client/native-client.constants';
 import {
   BasicResponseEnum,
   ConfigEntity,
-  ConfigEntityService,
   ConfigService,
+  UserEntity,
 } from '@huebot-hub-core/common';
 
 interface SetupJwtDecoded {
-  user: string;
+  user_id: string;
   public_key: string;
   secret: string;
 }
@@ -21,44 +22,64 @@ interface SetupJwtDecoded {
 export class InstanceService {
   constructor(
     @Inject(NATIVE_CLIENT_PROVIDER) private client: ClientProxy,
+    private readonly dataSource: DataSource,
     private readonly configService: ConfigService,
-    private readonly configEntityService: ConfigEntityService,
   ) {}
 
   public async setup(token: string): Promise<BasicResponseEnum> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
       const artifact_secret = this.configService.SECRET_KEY;
 
-      const { secret } = jwt.verify(token, artifact_secret) as SetupJwtDecoded;
+      const { user_id, public_key, secret } = jwt.verify(
+        token,
+        artifact_secret,
+      ) as SetupJwtDecoded;
 
-      const existing_instance_secret =
-        await this.configEntityService.repo.findOne({
+      const existing_instance_secret = await this.dataSource.manager.findOne(
+        ConfigEntity,
+        {
           where: {
             name: 'instance_secret',
           },
-        });
+        },
+      );
 
       if (existing_instance_secret) {
-        await this.configEntityService.repo.remove(existing_instance_secret);
+        await queryRunner.manager.remove(existing_instance_secret);
       }
 
       const new_instance = new ConfigEntity();
       new_instance.name = 'instance_secret';
       new_instance.value = secret;
-      await this.configEntityService.save(new_instance);
+      await queryRunner.manager.save(new_instance);
+
+      const new_user = new UserEntity();
+      new_user.user_id = user_id;
+      new_user.instance_public_key = public_key;
+      await queryRunner.manager.save(new_user);
 
       const ap_interface_response = await firstValueFrom(
         this.client.send<BasicResponseEnum>({ cmd: 'create_ap_interface' }, []),
       );
 
       if (ap_interface_response === BasicResponseEnum.FAIL) {
-        return BasicResponseEnum.FAIL;
+        throw Error('Failed to setup AP interface');
       }
+
+      await queryRunner.commitTransaction();
 
       return BasicResponseEnum.SUCCESS;
     } catch (error) {
       console.log('InstanceService.setup error: ', error);
+      await queryRunner.rollbackTransaction();
       return BasicResponseEnum.FAIL;
+    } finally {
+      await queryRunner.release();
     }
   }
 }
