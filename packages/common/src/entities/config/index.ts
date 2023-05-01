ece03d37fa854051953d99f1b690fe26271ffd1f
tyler smith <tyler.smith.la@gmail.com>
2023-05-01 12:16:01 +0000

    remove unused entities, update migration

--- packages/common/src/entities/config/index.ts
+++
@@ -1,3 +0,0 @@
-export { ConfigEntity } from './config.entity';
-export { ConfigEntityModule } from './config.module';
-export { ConfigEntityService } from './config.service';
--- packages/common/src/entities/index.ts
+++ packages/common/src/entities/index.ts
@@ -1,7 +1,1 @@
-export * from "./node"
-export * from "./config"
-export * from "./user"
-
-export {NodeChannelEntity} from "./node-channel/node-channel.entity"
-export {NodeChannelEntityModule} from "./node-channel/node-channel.module"
-export {NodeChannelEntityService} from "./node-channel/node-channel.service"
+export * from './user';
