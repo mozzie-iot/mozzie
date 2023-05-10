5ceef0a85f0328f46e023576ad04cec724e0cf3d
tyler smith <tyler.smith.la@gmail.com>
2023-05-10 19:02:33 +0000

    ci

--- scripts/deploy-tests.sh
+++ scripts/deploy-tests.sh
@@ -7,33 +7,32 @@ if [ $GIT_BRANCH != "devops" ]; then
     exit 1;
 fi
 
-echo "diffy"
+DIFF=$(yarn run diff)
 
-yarn run diff
+echo "DIFFFFF"
+echo $DIFF
 
-echo "DONE"
+COMMON_UPDATED=false
 
-# COMMON_UPDATED=false
+if echo $DIFF | grep -q "packages/common"; then
+    COMMON_UPDATED=true
+    echo "Detected updates in 'packages/common'. Running tests in all packages."
+fi
 
-# if echo $DIFF | grep -q "packages/common"; then
-#     COMMON_UPDATED=true
-#     echo "Detected updates in 'packages/common'. Running tests in all packages."
-# fi
+if [ $COMMON_UPDATED = true ] || echo $DIFF | grep -q "packages/api"; then
+    if $COMMON_UPDATED = false; then 
+        echo "Detected updates in 'packages/api'. Running tests."
+    fi
 
-# if [ $COMMON_UPDATED = true ] || echo $DIFF | grep -q "packages/api"; then
-#     if $COMMON_UPDATED = false; then 
-#         echo "Detected updates in 'packages/api'. Running tests."
-#     fi
+    if ! docker-compose -f docker-compose.test.yml up --exit-code-from api ; then
+        exit 1
+    fi
+fi
 
-#     if ! docker-compose -f docker-compose.test.yml up --exit-code-from api ; then
-#         exit 1
-#     fi
-# fi
+if [ $COMMON_UPDATED = false ] ||  echo $DIFF | grep -q "packages/mqtt"; then
+    if $COMMON_UPDATED = false; then 
+        echo "Detected updates in 'packages/mqtt'. Running tests."
+    fi
 
-# if [ $COMMON_UPDATED = false ] ||  echo $DIFF | grep -q "packages/mqtt"; then
-#     if $COMMON_UPDATED = false; then 
-#         echo "Detected updates in 'packages/mqtt'. Running tests."
-#     fi
-
-#     # docker-compose -f docker-compose.test.yml up --exit-code-from mqtt
-# fi
+    # docker-compose -f docker-compose.test.yml up --exit-code-from mqtt
+fi
