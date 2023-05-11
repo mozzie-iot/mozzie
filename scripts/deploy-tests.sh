487301fa7acef4504125381248de0716d4e84db1
tyler smith <tyler.smith.la@gmail.com>
2023-05-11 23:06:13 +0000

    ci

--- scripts/deploy-tests.sh
+++ scripts/deploy-tests.sh
@@ -7,53 +7,48 @@ if [ $GIT_BRANCH != "devops" ]; then
     exit 1;
 fi
 
-# set -e
+set -e
 
-echo "YARN RUN DIFF"
 DIFF=$(yarn run diff)
+
 echo $DIFF
-echo "END YARN RUN FIFG"
 
-# DIFF=$(yarn run diff)
+COMMON_UPDATED=false
 
-# echo $DIFF
+# Used for GH actions vars
+API_UPDATED=false
+MQTT_UPDATED=false
 
-# COMMON_UPDATED=false
+if echo $DIFF | grep -q "packages/common"; then
+    COMMON_UPDATED=true
+    echo "Detected updates in 'packages/common'. Running tests in all packages."
+fi
 
-# # Used for GH actions vars
-# API_UPDATED=false
-# MQTT_UPDATED=false
+if [ $COMMON_UPDATED = true ] || echo $DIFF | grep -q "packages/api"; then
+    if [ $COMMON_UPDATED = false ]; then 
+        echo "Detected updates in 'packages/api'. Running tests."
+    fi
 
-# if echo $DIFF | grep -q "packages/common"; then
-#     COMMON_UPDATED=true
-#     echo "Detected updates in 'packages/common'. Running tests in all packages."
-# fi
+    # if ! docker-compose -f docker-compose.test.yml up --exit-code-from api ; then
+    #     exit 1
+    # fi
 
-# if [ $COMMON_UPDATED = true ] || echo $DIFF | grep -q "packages/api"; then
-#     if [ $COMMON_UPDATED = false ]; then 
-#         echo "Detected updates in 'packages/api'. Running tests."
-#     fi
+    echo "API UPDATED"
 
-#     # if ! docker-compose -f docker-compose.test.yml up --exit-code-from api ; then
-#     #     exit 1
-#     # fi
+    API_UPDATED=true
+fi
 
-#     echo "API UPDATED"
+if [ $COMMON_UPDATED = true ] ||  echo $DIFF | grep -q "packages/mqtt"; then
+    if [ $COMMON_UPDATED = false ]; then 
+        echo "Detected updates in 'packages/mqtt'. Running tests."
+    fi
+    # export FOO=bar
+    MQTT_UPDATED=true
+    # docker-compose -f docker-compose.test.yml up --exit-code-from mqtt
+fi
 
-#     API_UPDATED=true
-# fi
+echo "UPDATED"
+echo $API_UPDATED
 
-# if [ $COMMON_UPDATED = true ] ||  echo $DIFF | grep -q "packages/mqtt"; then
-#     if [ $COMMON_UPDATED = false ]; then 
-#         echo "Detected updates in 'packages/mqtt'. Running tests."
-#     fi
-#     # export FOO=bar
-#     MQTT_UPDATED=true
-#     # docker-compose -f docker-compose.test.yml up --exit-code-from mqtt
-# fi
-
-# echo "UPDATED"
-# echo $API_UPDATED
-
-# # Set Github action variable
-# echo "api_updated=$API_UPDATED" >> $GITHUB_OUTPUT
+# Set Github action variable
+echo "api_updated=$API_UPDATED" >> $GITHUB_OUTPUT
