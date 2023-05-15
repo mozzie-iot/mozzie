3a65ed16613b01e04459c52db4f186c9d50dc971
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-15 11:51:37 -0400

    test: :white_check_mark: add user tests (#33)

--- packages/api/src/routes/core/user/user.controller.spec.ts
+++ packages/api/src/routes/core/user/user.controller.spec.ts
@@ -1,20 +1,21 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthModule } from '@huebot-api/auth/auth.module';
-import {
-  TypeOrmModule,
-  UserEntity,
-  UserEntityModule,
-} from '@huebot-hub-core/common';
+import { TypeOrmModule, UserEntityModule } from '@huebot-hub-core/common';
 
 import { UserController } from './user.controller';
-import { User } from './user.decorator';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
   let module: TestingModule;
   let userController: UserController;
-  let userService: UserService;
+
+  const input = {
+    username: 'test_user',
+    password: 'test_pw',
+    role: 'operator',
+  };
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
@@ -23,13 +24,81 @@ describe('UserController', () => {
       providers: [UserService],
     }).compile();
 
-    userService = module.get<UserService>(UserService);
     userController = module.get<UserController>(UserController);
   });
 
+  describe('create', () => {
+    it('successfully create new user', async () => {
+      expect(await userController.create(input)).toBe(undefined);
+    });
+
+    it('username already in use', async () => {
+      await expect(userController.create(input)).rejects.toThrowError(
+        new HttpException(
+          `Username '${input.username}' is already in use`,
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
+
+    it('unsupported role', async () => {
+      const input = {
+        username: 'clark',
+        password: 'kent',
+        role: 'superman',
+      };
+
+      await expect(userController.create(input)).rejects.toThrowError(
+        new HttpException(
+          `Unsupported role provided: ${input.role}`,
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
+
+    it("fail to create user with 'admin' role via endpoint", async () => {
+      const input = {
+        username: 'admin',
+        password: 'user',
+        role: 'admin',
+      };
+
+      await expect(userController.create(input)).rejects.toThrowError(
+        new HttpException(
+          "Users with 'admin' role must be created via CLI",
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
+  });
+
   describe('login', () => {
-    it('should return a null user', async () => {
-      expect(await userController.current_user(null)).toBe(null);
+    it('invalid username or password', async () => {
+      await expect(
+        userController.login({
+          username: input.username,
+          password: 'bad_password',
+        }),
+      ).rejects.toThrowError(
+        new HttpException(
+          'Invalid username or password!',
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
+
+    it('successful user login', async () => {
+      const login = await userController.login({
+        username: input.username,
+        password: input.password,
+      });
+
+      expect(login).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      );
     });
   });
 
