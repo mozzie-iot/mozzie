72e61db9983de9960ade852d53601279d2032a46
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-22 13:59:22 -0400

    Fix/install script (#48)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 
 exec 2>&1
 
+NON_ROOT_USER=$1
+
 INSTALL_DIR=/usr/local/bin
 LOG_STATUS=$INSTALL_DIR/huebot/.install
 LOG_FILE=$INSTALL_DIR/huebot/install.log
@@ -78,18 +80,28 @@ runInstall() {
 		error_found
 	fi
 
-	PACKAGES=$(apt-get install -y docker docker-compose ufw >> $LOG_FILE 2>&1)
+	PACKAGES=$(apt-get install -y ufw >> $LOG_FILE 2>&1)
 	
 	if ! $PACKAGES ; then
 		printf "Install packages failed\n"
 		error_found
 	fi
+
+	if ! curl -sSL https://get.docker.com | sh >> $LOG_FILE 2>&1 ; then 
+        printf "Install Docker failed\n"
+		error_found
+	fi
+
 	printf "Done.\n"
 
-	NON_ROOT_USER=$(logname)
 	printf "Add user (%s) to Docker group..." "${NON_ROOT_USER}"
 	if ! usermod -aG docker $NON_ROOT_USER >> $LOG_FILE 2>&1 ; then
 		printf "Failed: Error while adding user to Docker group.\n"
+		error_found
+	fi
+
+	if ! su $NON_ROOT_USER >> $LOG_FILE 2>&1 ; then
+		printf "Failed: Error while switching user to apply Docker group.\n"
 		error_found
 	fi
 	printf "Done.\n"
@@ -240,7 +252,7 @@ runInstall() {
 	printf "Done.\n"
 
 	printf "Installing and starting docker containers..."
-	if ! docker-compose -f $INSTALL_DIR/huebot/runner/docker-compose.yml up -d >> $LOG_FILE 2>&1 ; then
+	if ! docker compose -f $INSTALL_DIR/huebot/runner/docker-compose.yml up -d >> $LOG_FILE 2>&1 ; then
 		printf "Failed: Error while pulling/starting Docker containers"
 		error_found
 	fi
